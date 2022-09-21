026073d250df66ce40352fd83a35941c842de2ed
Rodrigo Vieira <rodrigo.vieira@maistopestetica.com.br>
2022-09-21 00:46:04 -0300

    Primeiro commit do app Orgs do curso de React Native da Alura

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+export default function app() {
+  return (
+    <View style={styles.container}>
+      <Text>Olá mundo!!!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+
+});
